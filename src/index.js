b9eaf741ae5c765e2b602736ebc4b0a076382fa0
ulifly <ulifly@hotmail.com>
2025-05-03 12:31:29 -0600

    servidor funcionando

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 
-
 import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hola compañerossss!');
+})
+
+app.listen(PORT, () => {
+    console.log(`servidor a la escucha en http://localhost:${PORT}`);
+})
+
